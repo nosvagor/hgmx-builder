8c11b704a75a5d001baf35b044842d477fbf1d66
cullyn <cullyn@nosvagor.com>
2025-08-03 14:12:13 -0700

    add(logos): icon logo, with duotone version (bw)

--- views/components/app/logo/full_templ.go
+++ views/components/app/logo/full_templ.go
@@ -8,9 +8,9 @@ package logo
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-var fullScriptHandle = templ.NewOnceHandle()
+var FullControllerHandler = templ.NewOnceHandle()
 
-func fullController() templ.Component {
+func FullController() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -49,7 +49,7 @@ func fullController() templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = fullScriptHandle.Once().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = FullControllerHandler.Once().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
