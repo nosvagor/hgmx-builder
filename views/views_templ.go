39bd79c9bca12a18e5c78ac86e04f29d7c3b477d
cullyn <cullyn@nosvagor.com>
2025-08-24 16:38:31 -0700

    new(favicon): logo svg added

--- views/views_templ.go
+++ views/views_templ.go
@@ -116,7 +116,7 @@ func Title(title string) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, " - hgmx</title>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, " | hgmx</title>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
